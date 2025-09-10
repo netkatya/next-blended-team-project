0b383eb34e0bd78771f84b141850aa5bc19d1479
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-09-10 13:46:41 +0100

    modif

--- lib/stores/exchangeStore.ts
+++ lib/stores/exchangeStore.ts
@@ -1,3 +1,6 @@
+import { exchangeCurrency, latestRates } from "../service/exchangeAPI";
+import { create } from "zustand";
+
 type ExchangeInfo = {
   to: string;
   from: string;
@@ -15,12 +18,41 @@ type ExchangeState = {
   convert: (data: { from: string; to: string; amount: number }) => Promise<void>;
 };
 
-export default function useExchangeStore = create<ExchangeState>((set, get) => ({
+export const useExchangeStore = create<ExchangeState>((set, get) => ({
     baseCurrency: "",
     exchangeInfo: null,
     rates: [],
     isLoading: false,
     isError: false,
 
-    setBaseCurrency: (currency) => set({baseCurrency: currency}),
+    setBaseCurrency: (currency) => set({ baseCurrency: currency }),
+    
+    convert: async (credentials) => {
+        try {
+            set({ isLoading: true, isError: false });
+            const data = await exchangeCurrency(credentials);
+            set({ exchangeInfo: data, isLoading: false });
+        } catch (err) {
+            console.error(err);
+            set({ isError: true, isLoading: false, exchangeInfo: null })
+        }
+    }
+    fetchRates: async () => {
+        const baseCurrency = get().baseCurrency || "USD";
+        try {
+            set({ isLoading: true, isError: false });
+            const rates = await latestRates(baseCurrency);
+
+            const filtered = rates.filter(([key]) => key !== baseCurrency)
+                .map(([key, value]) => ({
+                    key,
+                    value: (1 / value).toFixed(2),
+                }))
+            
+            set({ rates: filtered, isLoading: false });
+        } catch (err) {
+            console.error(err);
+            set({ isError: true, isLoading: false });
+        }
+    }
 }))
