fb676bbfeae3fd4764866874b9e9ab16c9487302
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-09-13 11:53:44 +0100

    fix

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,5 +1,5 @@
 import styled from './Container.module.css';
 
-export default function Container({ children }) {
+export default function Container({ children }: { children: React.ReactNode }) {
   return <div className={styled.container}>{children}</div>;
 }
